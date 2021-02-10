6a09c43b52d767e09977d1961ce4265d6f30f046
AldeiaCloud <78882441+aldeiacloud@users.noreply.github.com>
2021-02-10 19:08:03 -0300

    Script para instação da VPN PritUnl
    1- CRIAR ARQUIVO DE INSTALAÇÃO
    cd /tmp && touch install.sh && chmod +x install.sh && vim install.sh
    
    2- COLAR SCRIPT NO ARQUIVO
    
    3- EXECUTAR O SCRIPT
    ./install.sh
    
     -- AO LOGAR --
    sudo pritunl setup-key
    sudo pritunl default-password

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt update -y
+echo "deb http://repo.pritunl.com/stable/apt bionic main" | sudo tee /etc/apt/sources.list.d/pritunl.list
+touch /etc/apt/sources.list.d/mongodb-org-4.0.list
+echo "deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" >> /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt update -y
+sudo apt --assume-yes install pritunl mongodb-server
+sudo systemctl start pritunl mongodb
+sudo systemctl enable pritunl mongodb
+# CONFIGURAR HORARIO (BUENOS AIRES = BRASILIA SEM HORARIO DE VERAO)
+timedatectl set-timezone America/Argentina/Buenos_Aires
+# CRIAR SWAP
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
