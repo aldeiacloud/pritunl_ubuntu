8bf18780543b873eb7db2038394954f04c1dc72d
AldeiaCloud <78882441+aldeiacloud@users.noreply.github.com>
2021-03-13 00:57:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,4 @@ sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
