4aa75e0048f96db123d3ad8ea80a17cf09d37a81
AldeiaCloud <78882441+aldeiacloud@users.noreply.github.com>
2021-03-31 15:37:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-sudo apt update -y
-echo "deb http://repo.pritunl.com/stable/apt bionic main" | sudo tee /etc/apt/sources.list.d/pritunl.list
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb https://repo.pritunl.com/stable/apt bionic main
+EOF
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
 touch /etc/apt/sources.list.d/mongodb-org-4.0.list
 echo "deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" >> /etc/apt/sources.list.d/mongodb-org-4.0.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 sudo apt update -y
 sudo apt install pritunl mongodb-server -y
 sudo systemctl start pritunl mongodb
